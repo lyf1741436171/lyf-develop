c0583c938b5d868a8cf2a7aa6cad38ec5ded6b2a
liaoyangfan <liaoyangfan@freedotech.com>
2019-03-23 15:09:45 +0800

    提交代码

---
+++ lyf-feign/src/main/java/com/hudongyang/springcloud/servicefeign/ServiceFeignApplication.java
@@ -0,0 +1,19 @@
+package com.hudongyang.springcloud.servicefeign;
+
+import com.hudongyang.springcloud.servicefeign.feigninterface.HiInterface;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+
+//服务消费者
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(clients = {HiInterface.class})
+public class ServiceFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceFeignApplication.class, args);
+    }
+}
